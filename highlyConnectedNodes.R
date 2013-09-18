c75fd6b3c24825e6311a5adf7e77fcc7bbcbe7f3
Will Foran <willforan@gmail.com>
2013-09-18 11:30:43 -0400

    update poster, add posterpdf

--- highlyConnectedNodes.R
+++ highlyConnectedNodes.R
@@ -17,9 +17,14 @@ ageeff.highlyconn <- subset(ageeff.ageXphys,ROI1%in%node.connections$roinum|ROI2
 ageeff.highlyconn <- reshape(ageeff.highlyconn, direction = "long", varying = c('ROI1','ROI2'), sep = "")
 ageeff.highlyconn$ROIname <- gsub(' ?','',as.character(sapply(ageeff.highlyconn$ROI, function(x){ 
    roiidx<-which(roi.lables$num==x)
-   aname<-roi.lables$name[roiidx] 
+   xyz <- paste(collapse=" ",sep=" ",roi.lables[roiidx,c('x','y','z')]);
+    
+   aname<-gsub(' ?','',as.character(roi.lables$name[roiidx] ))
    if(aname=='unknown'){
     aname<-paste(sep="_",collapse="_",roi.lables[roiidx,c('x','y','z')] )
+   }
+   else {
+    aname<-paste(sep="_",aname,xyz)
    }
    as.character(aname)
    })))
@@ -33,8 +38,8 @@ for(i in 1:nrow(node.connections.orig)) {roi=node.connections.orig[i,'roinum']
  ageeff<-subset(ageeff,ROI1!=roi&ROI2!=roi)
  nsize=nrow(ageeff)
  idx <- which(roi.lables$num==roi)
- name= gsub(' ?','',as.character(roi.lables$name[idx]))
  xyz= paste(collapse=" ",sep=" ",roi.lables[idx,c('x','y','z')]);
+ name= paste(sep="_",gsub(' ?','',as.character(roi.lables$name[idx])),xyz)
  if((osize-nsize)/numcon<.5 || numcon<5){ next }
  cat(i,roi,numcon,osize-nsize,nrow(ageeff),"\n")
  hubs[[as.character(roi)]] <- c(name,numcon,osize-nsize,(osize-nsize)/numcon, xyz)
@@ -42,5 +47,5 @@ for(i in 1:nrow(node.connections.orig)) {roi=node.connections.orig[i,'roinum']
 
 #ageeff.highlyconn$ROI<-as.factor(ageeff.highlyconn$ROI)
 # %in% node.connections$roinum[1:10] vs %in% names(hubs)
-toptenhist <- ggplot(subset(ageeff.highlyconn,ROI %in% names(hubs)[1:13] ),aes(x=ageXphysio.val,fill=ROIname))+geom_histogram(stat="bin")+theme_bw()+scale_fill_brewer(type='qual',palette=3)
+toptenhist <- ggplot(subset(ageeff.highlyconn,ROI %in% names(hubs)[1:12] ),aes(x=ageXphysio.val,fill=ROIname))+geom_histogram(stat="bin")+theme_bw()+scale_fill_brewer(type='qual',palette='Spectral')
 ggsave(toptenhist, file='imgs/highlyconnected-histogram.svg')
